53b119220a659a8954115db605f8b5b7b7091e7e
Carrasco <136844452+Cocom1n@users.noreply.github.com>
2024-09-04 16:46:37 -0300

    Papupagina1
    prueba

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -3,9 +3,13 @@ import "./Inicio.css"
 function Inicio(){
     return(
         <div className="contenedorA">
-            <h1>Holaaa</h1>
-            <p>prueba</p>
-            <p>Updateros</p>
+            <section className="Titulo">
+                <h1>Hola Papus de papulandia</h1>
+                <p>Esta es nuestra primer papupagina estamos muy empocionados</p>
+                <p>Esperamos nos apoyen</p>
+                <img className="imagen" src="https://pbs.twimg.com/profile_images/1752441679213105152/i2rA03rP_400x400.jpg"></img>
+            </section>
+            
         </div>
     )
     
