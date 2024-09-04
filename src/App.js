0984c2f30f7f3809d602ffcb92df189a4060dd58
Carrasco <136844452+Cocom1n@users.noreply.github.com>
2024-09-04 16:20:06 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import  Inicio from "./components/Inicio.js";
+import "./App.css"
+
+function App (){
+    return(
+        <div>
+            
+            <Inicio />
+            
+        </div>
+    )
+}
+export default App;
---
+++ src/components/Inicio.js
@@ -0,0 +1,13 @@
+import "./Inicio.css"
+
+function Inicio(){
+    return(
+        <div className="contenedorA">
+            <h1>Holaaa</h1>
+            <p>prueba</p>
+            <p>Updateros</p>
+        </div>
+    )
+    
+}
+export default Inicio;
